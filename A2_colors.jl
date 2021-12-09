17d9649def3a42d14482ff897b29668e537dbb43
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-12-09 14:23:28 -0500

    🔥 remove color palette comparison figure

--- A2_colors.jl
+++ A2_colors.jl
@@ -2,5 +2,7 @@
 
 # Prepare colors
 p0 = colorant"#e8e8e8"
+bv_pal_1 = (p0=p0, p1=colorant"#64acbe", p2=colorant"#c85a5a")
 bv_pal_2 = (p0=p0, p1=colorant"#73ae80", p2=colorant"#6c83b5")
+bv_pal_3 = (p0=p0, p1=colorant"#9972af", p2=colorant"#c8b35a")
 bv_pal_4 = (p0=p0, p1=colorant"#be64ac", p2=colorant"#5ac8c8")
