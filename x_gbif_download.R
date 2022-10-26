f3e1f88c7c2c0f28aeef9db56c010fc9a81079e5
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2022-10-26 10:41:23 -0400

    🚚 move previous occurrence files into subfolder
    Previous files limited to 2000 occurrences have been replaced by complete ones but these are too large to be version controlled

--- x_gbif_download.R
+++ x_gbif_download.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
 # Load complete dataset
-occ <- read_tsv("./data/occurrences/all_occurrences.csv") # works now??
+occ <- read_tsv("./data/occurrences/subset/all_occurrences.csv") # works now??
 
 # Create a smaller data set
-write_csv(head(occ), "data/occurrences/all_occurrences_mini.csv")
+write_csv(head(occ), "data/occurrences/subset/all_occurrences_mini.csv")
 
 # Try to reexport
-write_tsv(head(occ, 100000), "data/occurrences/all_occurrences2.csv")
+write_tsv(head(occ, 100000), "data/occurrences/subset/all_occurrences2.csv")
 
 # Check exported dataset
-occ2 <- read_tsv("./data/occurrences/all_occurrences2.csv") # wrong number of rows??
+occ2 <- read_tsv("./data/occurrences/subset/all_occurrences2.csv") # wrong number of rows??
 head(occ, 100000) %>% nrow()
 
 # Investigate problems
@@ -18,10 +18,10 @@ problem_rows <- problems(occ2) %>% pull(row)
 problem_rows_buffered <- sort(c(problem_rows-1, problem_rows, problem_rows+1))
 problem_occ <- slice(occ, problem_rows_buffered)
 problem_occ
-write_tsv(problem_occ, "./data/occurrences/all_occurrences_problems.csv")
+write_tsv(problem_occ, "./data/occurrences/subset/all_occurrences_problems.csv")
 
 # Re-read problems?
-occ3 <- read_tsv("./data/occurrences/all_occurrences_problems.csv")
+occ3 <- read_tsv("./data/occurrences/subset/all_occurrences_problems.csv")
 all(occ3 == problem_occ, na.rm = TRUE)
 occ3 == problem_occ %>% View()
 occ3[which(occ3 != problem_occ)
@@ -52,7 +52,7 @@ sp_list <- unique(occ_select$name)
 # Export as separate files
 for (sp in sp_list) {
   sp_name <- str_replace(sp, " ", "_")
-  sp_path <- file.path("data", "occurrences2", paste0(sp_name, ".csv"))
+  sp_path <- file.path("data", "occurrences", paste0(sp_name, ".csv"))
   occ_select |>
     filter(name == sp) |>
     write_tsv(sp_path)
