a1112a5c81f9d0c7183b828e323b8f52e176565a
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2022-11-04 10:06:09 -0400

    🔥 move all cluster files to Narval

--- narval_test2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=ctb-tpoisot
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=4000M
-#SBATCH --time=00:60:00
-#SBATCH --job-name=test1
-#SBATCH --output=test1-%J.out
-module load StdEnv/2020 julia/1.8.1
-julia projects/def-tpoisot/2022-SpatialProbabilisticMetaweb/03_generate_sdms1.jl
