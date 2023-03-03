fb43cf528b12773f52950eca6adc705dbe1ae6c1
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-03-03 10:58:43 -0500

    🚧 attempt to balance file size between jobs

--- 02_get_absences.jl
+++ 02_get_absences.jl
@@ -25,7 +25,7 @@ occfiles = readdir(occ_path; join=true)
 filter!(!contains("all_occurrences"), occfiles) # remove backup files for QC data
 
 # Divide files if using job array
-if (@isdefined JOBARRAY) && JOBARRAY == true
+# if (@isdefined JOBARRAY) && JOBARRAY == true
     _jobid = parse(Int64, get(ENV, "SLURM_ARRAY_TASK_ID", "1"))
     _jobcount = parse(Int64, get(ENV, "SLURM_ARRAY_TASK_COUNT", "1"))
 
@@ -37,9 +37,46 @@ if (@isdefined JOBARRAY) && JOBARRAY == true
     _idx2 = _idx[_jobid+1]
 
     jobfiles = occfiles[_idx1:_idx2]
-else
-    jobfiles = occfiles
-end
+
+    # How inequal is the filesize between jobs?
+    gp0 = repeat(1:_jobcount, inner=_nfiles)[1:_ntot]
+    gp0_f = [occfiles[findall(==(i), gp0)] for i in 1:_jobcount]
+    gp0_fs = [sum(filesize.(f))/1000 for f in gp0_f]
+    extrema(gp0_fs)
+
+    # Balance larger files between jobs
+    gp = repeat(vcat(collect(1:_jobcount), collect(_jobcount:-1:1)), Int(_nfiles/2))[1:_ntot]
+    gp_f = [sort(occfiles, by=filesize, rev=true)[findall(==(i), gp)] for i in 1:_jobcount]
+    gp_fs = [sum(filesize.(f))/1000 for f in gp_f]
+    extrema(gp_fs)
+
+    # Balance files according to filesize
+    fs = filesize.(occfiles)
+    fs_rel = fs/sum(fs)
+    fs_cumu = cumsum(sort(fs_rel, rev=true))
+    fs_prop = fs_cumu/(1/_jobcount)
+    gp2 = round.(Int, fs_prop)
+    gp2_f = [sort(occfiles, by=filesize, rev=true)[findall(==(i), gp2)] for i in 1:_jobcount]
+    gp2_fs = [sum(filesize.(f))/1000 for f in gp2_f]
+    extrema(gp2_fs)
+
+    # Fix imbalance between two final groups
+    nmin = argmin(gp2_fs)
+    nmax = argmax(gp2_fs)
+    gp2_fs_max = filesize.(gp2_f[nmax])/1000
+    nmax_min = argmax(gp2_fs_max)
+    if maximum(gp2_fs) - minimum(gp2_fs) > maximum(gp2_fs_max)
+        _to_change = gp2_f[nmax][nmax_min]
+        gp2_f[nmin] = vcat(gp2_f[nmin], _to_change)
+        gp2_f[nmax] = filter(!=(_to_change), gp2_f[nmax])
+    end
+    gp2_fs = [sum(filesize.(f))/1000 for f in gp2_f]
+    extrema(gp2_fs)
+
+
+# else
+    obfiles = occfiles
+# end
 
 ispath(pa_path) || mkpath(pa_path)
 
