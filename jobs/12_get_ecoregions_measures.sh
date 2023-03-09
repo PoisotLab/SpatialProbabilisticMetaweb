11ecb608858e82a7b7e236d04868e097dd7b2770
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-03-09 17:29:55 -0500

    ✨ job script to create ecoregions

---
+++ jobs/12_get_ecoregions_measures.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=ctb-tpoisot
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=248G
+#SBATCH --time=06:00:00
+#SBATCH --job-name=12_get_ecoregions_measures.jl
+#SBATCH --output=jobs/job_12_get_ecoregions_measures-%J.out
+
+module load StdEnv/2020
+module load julia/1.8.1
+
+cd $HOME/scratch/2022-SpatialProbabilisticMetaweb
+julia --project --threads=63 -e 'CAN = true; include("12_get_ecoregions_measures.jl")'
