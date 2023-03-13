ad3e87788b32aa871865053ba08f1a9774e1fd27
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-03-13 13:53:26 -0400

    ✨ add ecoregion richness as figure option

--- 12_get_ecoregions_measures.jl
+++ 12_get_ecoregions_measures.jl
@@ -1,24 +1,18 @@
 #### Ecoregions
 
-# CAN = true
+CAN = true
 include("A0_required.jl")
 
 # Load the corresponding sdm results if dealing with QC or CAN data
 if (@isdefined CAN) && CAN == true
-    ref_path = joinpath("data", "input", "canada_ref_2.tif");
     eco_path = joinpath("data", "input", "canada_ecoregions.tif");
     results_path = joinpath("data", "results");
     ecoresults_path = joinpath("data", "ecoregions");
 else
-    ref_path = joinpath("data", "input", "quebec_ref_10.tif");
     eco_path = joinpath("data", "input", "quebec_ecoregions.tif")
     results_path = joinpath("xtras", "results");
     ecoresults_path = joinpath("xtras", "ecoregions");
 end
-
-# Define reference layer
-reference_layer = geotiff(SimpleSDMPredictor, ref_path)
-spatialrange = boundingbox(reference_layer)
 
 # Load ecoregions
 ecoregions = geotiff(SimpleSDMPredictor, eco_path)
@@ -32,9 +26,11 @@ ecoregions_stack = [replace(e, 0.0 => nothing) for e in ecoregions_stack]
 ## Basic summary statistics
 
 # Define the network measures to use
-network_measures = ["Co", "L", "Lv", "Ld"]
-network_fs = [connectance, links, links_var, linkage_density]
-network_filename = ["connectance", "links_mean", "links_var", "links_density"]
+measures = ["Co", "L", "Lv", "Ld", "S", "Sσ"]
+filenames = [
+    "connectance", "links_mean", "links_var", "links_density",
+    "richness_mean", "richness_uncertainty"
+]
 
 # Define the summary functions we will use
 quantile055(x) = quantile(x, 0.055)
@@ -42,20 +38,11 @@ quantile945(x) = quantile(x, 0.945)
 iqr89(x) = quantile945(x) - quantile055(x)
 summary_fs = [median, quantile055, quantile945, iqr89]
 
-# Predefine set of options for threading
-opt = []
-for (m, fn) in zip(network_measures, network_fs), fm in summary_fs
-    o = (m = m, fn = fn, fm = fm)
-    push!(opt, o)
-end
-
 # Load layers to summarize by ecoregion
 local_layers = Dict{String, SimpleSDMPredictor}()
-for (m, f) in zip(network_measures, network_filename)
+for (m, f) in zip(measures, filenames)
     local_layers[m] = geotiff(SimpleSDMPredictor, joinpath(results_path, "$f.tif"))
 end
-local_layers["S"] = geotiff(SimpleSDMPredictor, joinpath(results_path, "richness_mean.tif"))
-local_layers["Sσ"] = geotiff(SimpleSDMPredictor, joinpath(results_path, "richness_uncertainty.tif"))
 
 # Define function
 function ecoregionalize(layer, ecoregions_stack; f=mean, keepzeros=true)
@@ -69,18 +56,27 @@ function ecoregionalize(layer, ecoregions_stack; f=mean, keepzeros=true)
     return l_eco
 end
 
+# Predefine set of options
+opt = []
+for m in measures, fs in summary_fs
+    o = (m = m, fs = fs)
+    push!(opt, o)
+end
+opt
+
 # Summarize by ecoregion
 ecoregion_layers = Dict{String, SimpleSDMResponse}()
 for o in opt
-    ecoregion_layers["$(o.m)_$(o.fm)"] = ecoregionalize(
-        local_layers[o.m], ecoregions_stack; f=o.fm
+    ecoregion_layers["$(o.m)_$(o.fs)"] = ecoregionalize(
+        local_layers[o.m], ecoregions_stack; f=o.fs
     )
 end
+ecoregion_layers
 
 # Export layers
 isdir(ecoresults_path) || mkdir(ecoresults_path)
 for o in opt
-    p = joinpath(ecoresults_path, "ecoregion_$(o.m)_$(o.fm).tif")
-    l = ecoregion_layers["$(o.m)_$(o.fm)"]
+    p = joinpath(ecoresults_path, "ecoregion_$(o.m)_$(o.fs).tif")
+    l = ecoregion_layers["$(o.m)_$(o.fs)"]
     geotiff(p, l)
 end
--- 13_plot_ecoregions.jl
+++ 13_plot_ecoregions.jl
@@ -17,25 +17,26 @@ isdir(fig_path) || mkdir(fig_path)
 
 # Define the network measures to use
 network_measures = ["Co", "L", "Lv", "Ld"]
-network_fs = [connectance, links, links_var, linkage_density]
-network_filename = ["connectance", "links_mean", "links_var", "links_density"]
+measures = [network_measures..., "S", "Sσ"]
 summary_fs = ["median", "quantile055", "quantile945", "iqr89"]
 summary_ts = ["median", "5.5% quantile", "94.5% quantile", "89% IQR"]
 
 # Predefine set of options
 opt = []
-for (m, fn) in zip(network_measures, network_fs), fm in summary_fs
-    o = (m = m, fn = fn, fm = fm)
+for m in measures, fs in summary_fs
+    o = (m = m, fs = fs)
     push!(opt, o)
 end
+opt
 
 # Load the ecoregion summary layers
 ecoregion_layers = Dict{String, SimpleSDMResponse}()
 for o in opt
-    ecoregion_layers["$(o.m)_$(o.fm)"] = geotiff(
-        SimpleSDMResponse, joinpath(ecoresults_path, "ecoregion_$(o.m)_$(o.fm).tif")
+    ecoregion_layers["$(o.m)_$(o.fs)"] = geotiff(
+        SimpleSDMResponse, joinpath(ecoresults_path, "ecoregion_$(o.m)_$(o.fs).tif")
     )
 end
+ecoregion_layers
 
 ## Make some plots!!
 
@@ -46,12 +47,9 @@ plot(
 )
 savefig(joinpath(fig_path, "ecoregion_all_median.png"))
 
-# plot(ecoregion_layers["S"]; title="S")
-# plot(ecoregion_layers["Sσ"]; title="Sσ")
-
 # Some variations
 ecoregion_plots = Dict{String, Plots.Plot}()
-for m in network_measures
+for m in measures
     begin
         _L = [ecoregion_layers["$(m)_$f"] for f in summary_fs]
         clim1 = mapreduce(minimum, min, _L)
@@ -69,3 +67,12 @@ ecoregion_plots["Co"]
 ecoregion_plots["L"]
 ecoregion_plots["Lv"]
 ecoregion_plots["Ld"]
+ecoregion_plots["S"]
+ecoregion_plots["Sσ"]
+
+## Compare with richness
+plot(
+    [plot(ecoregion_layers["$(m)_median"]) for m in ["L", "Lv", "S", "Sσ"]]...;
+    title=["Links" "Link variance" "Richness" "Richness variance"],
+)
+savefig(joinpath(fig_path, "ecoregion_comparison.png"))
