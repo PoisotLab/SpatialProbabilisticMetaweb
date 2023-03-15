5572625960137871b7e4ccbb677988cd55f338a9
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-03-15 16:19:05 -0400

    🚚 move ecoregion job script for extra metaweb script

--- jobs/x_get_ecoregions_metaweb.sh
+++ jobs/x_get_ecoregions_metaweb.sh
@@ -4,11 +4,11 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=450G
 #SBATCH --time=04:30:00
-#SBATCH --job-name=12_get_ecoregions_measures.jl
-#SBATCH --output=jobs/job_12_get_ecoregions_measures-%J.out
+#SBATCH --job-name=x_get_ecoregions_metaweb.jl
+#SBATCH --output=jobs/job_x_get_ecoregions_metaweb-%J.out
 
 module load StdEnv/2020
 module load julia/1.8.1
 
 cd $HOME/scratch/2022-SpatialProbabilisticMetaweb
-julia --project --threads=63 -e 'CAN = true; include("12_get_ecoregions_measures.jl")'
+julia --project --threads=63 -e 'CAN = true; include("x_get_ecoregions_measures.jl")'
