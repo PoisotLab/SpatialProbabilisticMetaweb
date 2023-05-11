3eb3663badccd984d8e4bb0eb7a31f192694255b
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-05-11 16:08:25 -0400

    🔧 temporary update to load script

--- A0_required.jl
+++ A0_required.jl
@@ -1,6 +1,7 @@
 import Pkg; Pkg.activate(".")
 
 import CSV
+using CairoMakie
 using Chain
 using DataFrames
 using DataFramesMeta
@@ -11,21 +12,14 @@ using GDAL
 using ProgressMeter
 using Random
 using Shapefile
-using SimpleSDMLayers
+using SpeciesDistributionToolkit
 using Statistics
 using StatsBase
-using StatsPlots
 using Base.Threads: @threads
 using JLD2, CodecZlib
 import ColorBlendModes
-using Plots.PlotMeasures
-
-import Plots.plot
-import SimpleSDMLayers.bivariate
-
-default(; dpi=300)
 
 include("A1_LCBD.jl")
-include("A2_colors.jl")
-include("A3_bivariate_values.jl")
-include("A4_worldshape.jl")
+# include("A2_colors.jl")
+# include("A3_bivariate_values.jl")
+# include("A4_worldshape.jl")
