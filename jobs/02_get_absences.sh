ea1490627a86a241e5343ce9c8fb14a196fdbd84
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2023-11-15 14:38:41 -0500

    🔧 update job scripts for DistanceToEvent pseudoabsences

--- jobs/02_get_absences.sh
+++ jobs/02_get_absences.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --account=ctb-tpoisot
-#SBATCH --array=1-3
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=100G
-#SBATCH --time=08:00:00
-#SBATCH --job-name=02_get_absences
-#SBATCH --output=jobs/out/job_02_get_absences-%J.out
+#SBATCH --mem=248G
+#SBATCH --time=04:00:00
+#SBATCH --job-name=x_get_absences_WithinRadius
+#SBATCH --output=jobs/out/job_%x-%J.out
 
 module load StdEnv/2020
 module load julia/1.9.1
 
 cd $HOME/projects/def-tpoisot/2022-SpatialProbabilisticMetaweb/
-julia --project --threads=63 -e 'JOBARRAY = true; CAN = true; WITHIN_RADIUS = true; quiet = true; include("02_get_absences.jl")'
+julia --project --threads=63 -e 'CAN = true; quiet = true; include("02_get_absences.jl")'
